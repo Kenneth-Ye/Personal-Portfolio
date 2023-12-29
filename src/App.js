21fc8b1186dc3f747d1e19b8d237d664a498f9a0
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2023-12-29 02:36:33 -0500

    build about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/layout'
 import Home from './components/home/home'
+import About from './components/about/about'
 function App() {
   return (
     <Routes>
       <Route>
         <Route path="/Personal-Portfolio" element={<Layout/>}>
           <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
         </Route>
       </Route>
     </Routes>
---
+++ src/components/about/about.js
@@ -0,0 +1,12 @@
+import './about.scss'
+
+const About = () => {
+    return (
+        <div className="container about-page">
+            <div className="text-zone">
+                 
+            </div>
+        </div>
+    )
+}
+export default About;
