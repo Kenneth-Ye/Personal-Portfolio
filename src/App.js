9ce6e7af583ae60d667fc56aa72aac5162cb91a7
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2023-12-29 19:47:07 -0500

    fix spa 404 issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/home/home'
 import About from './components/about/about'
 function App() {
   return (
-    <Routes>
+    <Routes basename="/Personal-Portfolio">
         <Route path="/Personal-Portfolio" element={<Layout/>}>
           <Route index element={<Home/>}/>
           <Route path="about" element={<About/>}/>
