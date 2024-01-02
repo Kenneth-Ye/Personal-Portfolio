eb64b850347a34e3c20a02ce3e66772bf1bc7430
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2024-01-01 20:12:57 -0500

    build contact page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Layout from './components/Layout/layout'
 import Home from './components/home/home'
 import About from './components/about/about'
 import Skills from './components/skills/skills'
+import Contact from './components/contact/contact'
+
 function App() {
   return (
     <Routes basename="/Personal-Portfolio">
@@ -11,6 +13,7 @@ function App() {
           <Route index element={<Home/>}/>
           <Route path="about" element={<About/>}/>
           <Route path="skills" element={<Skills/>}/>
+          <Route path="contact" element={<Contact/>}/>
         </Route>
     </Routes>
   );
