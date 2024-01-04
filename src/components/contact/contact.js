a94b63428ae70c182694b47299ffb0b0c0939d97
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2024-01-03 23:44:37 -0500

    build contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,10 +1,33 @@
+import AnimatedLetters from '../animateLetters/animateLetters';
 import './contact.scss';
+import {useState, useEffect} from 'react';
+import Loader from 'react-loaders';
+
 
 const Contact = () => {
+    const strArray = "Contact Me".split("");
+    const [letterClass, setLetterClass] = useState('text-animate');
+    
+    useEffect(() => {
+        const timeout =  setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000);
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, [])
+
     return (
-        <div className="contact-container">
-            Hello
+        <>
+        <div className="container contact-page">
+            <div className="text-zone">
+                <h1>
+                    <AnimatedLetters strArray={strArray} letterClass={letterClass} idx={12}/>
+                </h1>
+            </div>
         </div>
+        <Loader type="line-scale-pulse-out"/>
+        </>
     )
 }
 
