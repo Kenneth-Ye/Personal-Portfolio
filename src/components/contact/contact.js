681cae505ec7b87052194bfa643cffe1a3e6a33c
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2024-01-06 00:23:20 -0500

    connect emailjs

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,13 +1,15 @@
 import AnimatedLetters from '../animateLetters/animateLetters';
 import './contact.scss';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import Loader from 'react-loaders';
+import emailjs from '@emailjs/browser'
 
 
 const Contact = () => {
     const strArray = "Contact Me".split("");
     const [letterClass, setLetterClass] = useState('text-animate');
-    
+    const refForm = useRef()
+
     useEffect(() => {
         const timeout =  setTimeout(() => {
             setLetterClass('text-animate-hover')
@@ -17,6 +19,24 @@ const Contact = () => {
         }
     }, [])
 
+    const sendEmail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm(
+            process.env.REACT_ENV_YOUR_SERVICE_ID,
+            process.env.REACT_ENV_YOUR_TEMPLATE_ID,
+            refForm.current,
+            process.env.REACT_ENV_YOUR_KEY
+        )
+        .then(
+            () => {
+                alert('Message sent succesfully!')
+            },
+            () => {
+                alert('Failed to send message, please try again.')
+            }
+        )
+    }
+
     return (
         <>
         <div className="container contact-page">
@@ -25,7 +45,7 @@ const Contact = () => {
                     <AnimatedLetters strArray={strArray} letterClass={letterClass} idx={12}/>
                 </h1>
                 <div className="contact-form">
-                    <form>
+                    <form ref={refForm} onSubmit={sendEmail}>
                         <ul>
                             <li className='half'>
                                 <input type="text" name="name" placeholder='Name' required/>
@@ -37,7 +57,7 @@ const Contact = () => {
                                 <input type="text" name="subject" placeholder='Subject' required/>
                             </li>
                             <li>
-                                <textarea name="subject" placeholder='Message' required/>
+                                <textarea name="message" placeholder='Message' required/>
                             </li>
                             <li>
                                 <input type="submit" className='flat-button' value="SEND"/>
